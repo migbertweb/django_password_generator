f29f51743ea305b83b3bcdb70300a592815cf455
migbertweb <migbertyanez@disroot.org>
2023-04-15 18:17:25 -0300

    mejorando la seguridad con un .env

--- app/django_password_generator/settings.py
+++ app/django_password_generator/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -19,14 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+DEBUG = int(os.environ.get("DEBUG", default=0))
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nerb%q@xmq07e8=8hqi4(sd^$t-gy0y)-y@k#fk7(acbjjj!8s'
+# SECRET_KEY = 'django-insecure-v)=3y85hc*5)bg8r#h+fuxr_k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+# DEBUG = True
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+CSRF_TRUSTED_ORIGINS = ['http://localhost:*', 'http://127.0.0.1:*']
 
 # Application definition
 
